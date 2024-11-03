0fe77cea30a0dc5752627a60044f89470e2da0c5
Carlos Ezequiel Romero <ezequielromero3039@gmail.com>
2024-11-03 15:05:07 -0300

    fix: Merge errors are fixed

--- client/src/preceptor/routes/PreceptorRoutes.jsx
+++ client/src/preceptor/routes/PreceptorRoutes.jsx
@@ -1,22 +1,16 @@
 import { Route, Routes } from "react-router-dom"
-<<<<<<< HEAD
 import HomePage from '../pages/HomePage';
 import {PreceptorCalendar} from "../pages/PreceptorAsistencias"
-=======
 import { PreceptorPage } from "../pages/PreceptorPage";
 import { PreceptorCreate } from "../pages/PreceptorCreate";
->>>>>>> develop-Ezequiel
 
 export const PreceptorRoutes = () => {
     return(
         <Routes>
-<<<<<<< HEAD
             <Route path="/preceptor" element={<HomePage />}/>
             <Route path="/preceptor/asistencias" element={<PreceptorCalendar />}/>
-=======
             <Route path="/preceptores/" element={<PreceptorPage />}/>
             <Route path="/preceptores/create/" element={<PreceptorCreate />}/>
->>>>>>> develop-Ezequiel
         </Routes>
     )
 }

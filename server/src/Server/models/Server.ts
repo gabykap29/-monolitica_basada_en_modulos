6b8eff86f5744c067f542eabce0d3515e15bda9c
AgustinM12 <agustinmazza12345@gmail.com>
2024-11-03 02:30:42 -0300

    feat: se añadio la funcion para marcar ausentes automaticamente

--- server/src/Server/models/Server.ts
+++ server/src/Server/models/Server.ts
@@ -8,14 +8,11 @@ import authRouter from '../../Auth/routes/auth.routes';
 import attendanceRouter from '../../Attendance/router/Attendance.routes';
 import { userInitial } from '../helpers/userInitial';
 import cron from 'node-cron';
-import dayjs from 'dayjs';
 import { AttendanceService } from '../../Attendance/service/Attendance.services';
-import UserService from '../../Users/service/UserService';
-import { CustomError } from '../helpers/customError';
 import { loggerMiddleware } from '../middlewares/winston';
 
 const attendanceService = new AttendanceService();
-const userService = new UserService();
+
 
 class Server {
   private app: Application;
@@ -49,34 +46,13 @@ class Server {
     cron.schedule('15 9 * * *', async () => {
       console.log('Ejecutando la tarea de marcar ausentes a las 9:15 AM');
       try {
-        const todayDate = dayjs().format('YYYY-MM-DD');
-
-        // TODO: Encuentra a los estudiantes que no tienen un registro de "presente" para el día actual
-        const students = await userService.getAllUser('stundent');
-        const presentStudents = await attendanceService.findAllByDate({
-          date: todayDate,
-        });
-
-        if (
-          typeof presentStudents !== 'boolean' &&
-          !(students instanceof CustomError)
-        ) {
-          const absentStudent = students.filter(
-            (student) =>
-              !presentStudents.some(
-                (present) => present.idStudent === student._id,
-              ),
-          );
-
-          // TODO: Agrega el estado de "ausente" para cada estudiante sin asistencia en el día actual
-
-          console.log('Registro de ausentes completado');
-        }
+        attendanceService.markAbsent();
       } catch (error) {
         console.error('Error al registrar ausentes:', error);
       }
     });
   }
+
 
   public listen(): void {
     this.app.listen(this.port, '0.0.0.0', async () => {
